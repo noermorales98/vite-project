70ee92d5defd0977e56ec7a8577f46cac1f76018
Noeli <noe.rmorales98@gmail.com>
2023-03-22 19:27:09 -0500

    componentes funcionales 4.6

--- src/components/Calculadora.jsx
+++ src/components/Calculadora.jsx
@@ -1,61 +1,42 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class Calculadora extends Component {
-    //primer paso del ciclo de vida
-    constructor(props) {
-        super(props)
-        this.state = {
-            num1: 0,
-            num2: 0
-        }
-        this.setNumber1 = this.setNumber1.bind(this)
-        this.setNumber2 = this.setNumber2.bind(this)
-    }
+const Calculadora = (props) => {
 
-    setNumber1(e) {
-        this.setState({
-            ...this.state,
+    const [state, setState] = useState({
+        num1: 0,
+        num2: 0
+    })
+    
+    const setNumber1 = (e) =>{
+        setState({
+            ...state,
             num1: e.target.value
         })
     }
-
-    setNumber2(e) {
-        this.setState({
-            ...this.state,
+    
+    const setNumber2 = (e) =>{
+        setState({
+            ...state,
             num2: e.target.value
         })
+    }
 
-    }
-    
-    //segundo paso del ciclo de vida
-
-    render() {
-        return (
-            <div>
-                <h2>{this.props.name}</h2>
-                <form>
-                    <input type="number" name="numero1" id="numero1" value={this.state.num1}
-                        onChange={this.setNumber1}
-                    />
-                    <input type="number" name="numero2" id="numero2" value={this.state.num2}
-                        onChange={this.setNumber2}
-                    />
-                    <input type="button" value="Calcular" />
-                </form>
-                <span>resultado : {Number(this.state.num1) + Number(this.state.num2)}</span>
-            </div>
-        );
-    }
-    
-    //tercer paso del ciclo de vida
-    componentDidMount(){
-        
-    }
-    
-    componentWillUnmount(){
-        
-    }
-    
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <form>
+                <input type="number" name="numero1" id="numero1" value={state.num1}
+                    onChange={setNumber1}
+                />
+                <input type="number" name="numero2" id="numero2" value={state.num2}
+                    onChange={setNumber2}
+                />
+                <input type="button" value="Calcular" />
+            </form>
+            <span>resultado : {Number(state.num1) + Number(state.num2)}</span>
+        </div>
+    );
 }
+
 
 export default Calculadora;
