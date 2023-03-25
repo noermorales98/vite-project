5c5c2720883223b3f951c4606eab471786c96f0b
Noeli <noe.rmorales98@gmail.com>
2023-03-25 16:45:02 -0500

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <main>
+          <h1 className='font-bold text-6xl'>Analiza el mercado de las criptomonedas</h1>
+          <p className='text-xl'>Obtén información sobre las tendencias de precios, volumen y capitalización del mercado de las criptomonedas y descubre las mejores oportunidades de inversión en criptomonedas.</p>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
