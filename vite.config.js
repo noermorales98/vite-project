57834fa0e0462e530480086e711f618e31050178
Noeli <noe.rmorales98@gmail.com>
2023-07-08 16:23:48 -0500

    a

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
   base: '/vite-project/',
   resolve:{
     alias:{
-      '@cryptos': '/src/pages/cryptos/Cryptos.jsx',
-      '@crypto': '/src/pages/cryptos/Crypto.jsx',
-      '@home': '/src/pages/home/Home'
+      '@cryptos': 'src/pages/cryptos/Cryptos.jsx',
+      '@crypto': 'src/pages/cryptos/Crypto.jsx',
+      '@home': 'src/pages/home/Home'
     }
   }
 })
