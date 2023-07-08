087e1f19799042cdac5193d93e3b07033d9c5fe7
Noeli <noe.rmorales98@gmail.com>
2023-07-08 16:18:56 -0500

    @crystos

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/main.css";
 import "./index.css";
-import CryptosPage from "./pages/cryptos/Cryptos";
+import CryptosPage from "@cryptos";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Error from "./pages/error/error";
 import App from "./App.jsx";
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project/'
+  base: '/vite-project/',
+  resolve:{
+    alias:{
+      '@cryptos': '/src/pages/cryptos/Cryptos.jsx',
+    }
+  }
 })
