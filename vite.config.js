1b5f3922f4d12c28803ffabbf0171925665a3449
Noeli <noe.rmorales98@gmail.com>
2023-07-08 20:05:08 -0500

    seg

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/vite-project/',
-  resolve: { preserveSymlinks: true }
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+   }
 })
